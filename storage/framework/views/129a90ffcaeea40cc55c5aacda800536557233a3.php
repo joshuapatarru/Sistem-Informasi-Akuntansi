6cb401bcffbdafb319b16eff5c1056963857271f
joshuapatarru <patarruj@gmail.com>
2023-12-02 12:34:14 +0700

    first commit

---
+++ storage/framework/views/129a90ffcaeea40cc55c5aacda800536557233a3.php
@@ -0,0 +1,3 @@
+<footer class="footer">
+    <?php echo $__env->make('layouts.footers.nav', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+</footer><?php /**PATH C:\Users\LENOVO\OneDrive\Documents\joshua\akuntansi\resources\views/layouts/footers/auth.blade.php ENDPATH**/ ?>
